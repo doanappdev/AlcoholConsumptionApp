fb9a667bc805ac3d6ba82916e97149e1a1f058a9
DoanAppDev <doanappdev@gmail.com>
2014-09-16 15:52:55 +1000

    update number 2

---
+++ DrugAndAlcoholApp/app/src/main/java/uc/edu/itp/drugandalcohol/fragments/EULAFragment.java
@@ -0,0 +1,33 @@
+package uc.edu.itp.drugandalcohol.fragments;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uc.edu.itp.drugandalcohol.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class EULAFragment extends Fragment {
+
+
+    public EULAFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eula, container, false);
+    }
+
+
+}
